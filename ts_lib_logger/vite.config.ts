066d474b100b2137be4d1c13771ee8816a6d21a9
shaas99 <ashutosh.sharma@bluejeans.com>
2023-02-27 13:06:33 +0530

    added vite config updated package.json

---
+++ ts_lib_logger/vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ts_lib_logger',
+      fileName: 'ts_lib_logger',
+    },
+  },
+  plugins: [dts()],
+});
