e897c03e5f880db06d5fde12c7cf1eb6d7c042e0
shaas99 <ashutosh.sharma@bluejeans.com>
2023-03-03 18:07:17 +0530

    added logging msgs to button.tsx

--- react_lib_comps/src/Button.tsx
+++ react_lib_comps/src/Button.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { MouseEvent } from "react";
 
+
+import { logging } from 'ts_lib_logger';
+
+
+logging
+  .configure({
+    minLevels: {
+    '': 'error',
+    'Button': 'info'
+    }
+  })
+  .registerConsoleLogger();
+
+
+const logger = logging.getLogger('Button');
+
+
+
+
 export interface ButtonProps {
     text: string;
     action: ()=>void;
 }
 
 const Button = (props : ButtonProps) => {
+
+    logger.info(`Button with text ${props.text} rendered`);
+
     return <button onClick={() => props.action()}>{props.text}</button>;
 }
 
