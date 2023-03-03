f03439e845f4fa8bed236af8cf725be298c18feb
shaas99 <ashutosh.sharma@bluejeans.com>
2023-03-03 16:38:56 +0530

    vite.config.ts changed

--- react_lib_comps/vite.config.ts
+++ react_lib_comps/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+// https://vitejs.dev/guide/build.html#library-mode
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'my-lib',
+      fileName: 'my-lib',
+    },
+  },
+  plugins: [react(), dts()],
 })
+
+
