34b2b34041624e0f2a533a031f95930ed65d07f9
shaas99 <ashutosh.sharma@bluejeans.com>
2023-03-03 18:07:45 +0530

    added logging to TextField.tsx

--- react_lib_comps/src/TextField.tsx
+++ react_lib_comps/src/TextField.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 
+import { logging } from 'ts_lib_logger';
+
+
+logging
+  .configure({
+    minLevels: {
+    '': 'error',
+    'TextField': 'info'
+    }
+  })
+  .registerConsoleLogger();
+
+
+const logger = logging.getLogger('TextField');
+
+
 export interface TextFieldProps {
     value: number;
     setValue: (arg0: number) => void
 }
 
 const TextField = ({value , setValue}: TextFieldProps) => {
+
+    logger.info(`TextField with value ${value} rendered`);
+
     return <input type="number" value={value} onChange={(e) => setValue(parseInt(e.target.value))}></input>;
 }
 
